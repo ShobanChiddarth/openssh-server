ea51cb837dab57f161b842d099cd6c04a3ccadcc
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-06 14:22:51 +0530

    Added alpine-3.23.3 build files

---
+++ alpine-3.23.3/ssh-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+# Validate required vars
+: "${SSH_USER:?SSH_USER is required}"
+: "${SSH_PASSWORD:?SSH_PASSWORD is required}"
+: "${SSH_PUBLIC_KEY:?SSH_PUBLIC_KEY is required}"
+
+# Create user if doesn't exist
+if ! id "$SSH_USER" >/dev/null 2>&1; then
+    adduser -D -s /bin/sh "$SSH_USER"
+fi
+
+# Set password
+echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+
+# Set up authorized_keys
+USER_HOME=$(getent passwd "$SSH_USER" | cut -d: -f6)
+mkdir -p "$USER_HOME/.ssh"
+echo "$SSH_PUBLIC_KEY" > "$USER_HOME/.ssh/authorized_keys"
+chmod 700 "$USER_HOME/.ssh"
+chmod 600 "$USER_HOME/.ssh/authorized_keys"
+chown -R "$SSH_USER:$SSH_USER" "$USER_HOME"
+
+ssh-keygen -A
+
+exec /usr/sbin/sshd -D
